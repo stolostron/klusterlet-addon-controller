3a36b7730235e30c910b62ad5acf627c4c5f7c09
Ray Harris <harrisra@us.ibm.com>
2019-07-02 14:21:28 -0400

    Update Connection Manager Spec

--- pkg/apis/klusterlet/v1alpha1/klusterletservice_types.go
+++ pkg/apis/klusterlet/v1alpha1/klusterletservice_types.go
@@ -2,6 +2,7 @@ package v1alpha1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/kubernetes/pkg/apis/core"
 )
 
 // IMPORTANT: Run "operator-sdk generate k8s" to regenerate code after modifying this file
@@ -10,18 +11,57 @@ import (
 // +k8s:openapi-gen=true
 type KlusterletServiceSpec struct {
 	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
-	ConnectionManager ConnectionManager `json:"connection-manager"`
-	CertificateIssuer CertificateIssuer `json:"certificate-issuer"`
-	Search            Search            `json:"search"`
-	WorkManager       WorkManager       `json:"work-manager"`
-	PolicyController  PolicyController  `json:"policy-controller"`
-	ServiceRegistry   ServiceRegistry   `json:"service-registry"`
-	TopologyCollector TopologyCollector `json:"topology-collector"`
+	// +kubebuilder:validation:MinLength=1
+	Version string `json:"version"`
+	// +kubebuilder:validation:MinLength=1
+	Namespace string `json:"namespace"`
+
+	ConnectionManager ConnectionManager `json:"connectionManager"`
+	CertificateIssuer CertificateIssuer `json:"certificateIssuer"`
+	Search            Search            `json:"search,omitempty"`
+	WorkManager       WorkManager       `json:"workManager,omitempty"`
+	PolicyController  PolicyController  `json:"policyController,omitempty"`
+	ServiceRegistry   ServiceRegistry   `json:"serviceRegistry,omitempty"`
+	TopologyCollector TopologyCollector `json:"topologyCollector,omitempty"`
+}
+
+// Image defines the image to pull for a container
+// +k8s:openapi-gen=true
+type Image struct {
+	// +kubebuilder:validation:MinLength=1
+	Repository string `json:"repository"`
+	// +kubebuilder:validation:MinLength=1
+	Tag string `json:"tag"`
+	// +kubebuilder:validation:Enum=Always,Never,IfNotPresent
+	PullPolicy core.PullPolicy `json:"pullPolicy"`
+}
+
+// Hub defines an MCM hub for the ConnectionManager component
+type Hub struct {
+	// +kubebuilder:validation:MinLength=1
+	Name string `json:"name"`
+	// +kubebuilder:validation:MinLength=1
+	Secret core.LocalObjectReference `json:"secret"`
 }
 
 // ConnectionManager defines the desired state of the ConnectionManager component
 // +k8s:openapi-gen=true
 type ConnectionManager struct {
+	Enabled bool `json:"enabled"`
+
+	// +kubebuilder:validation:MinLength=1
+	ClusterName string `json:"clusterName"`
+	LogLevel    int    `json:"logLevel"`
+	Replicas    int    `json:"replicas"`
+
+	// +kubebuilder:validation:MinLength=1
+	Hubs []Hub `json:"hubs"`
+
+	Affinity        core.Affinity             `json:"affinity"`
+	Image           Image                     `json:"image"`
+	ImagePullSecret core.LocalObjectReference `json:"imagePullSecret"`
+	NodeSelector    core.NodeSelector         `json:"nodeSelector"`
+	Tolerations     []core.Toleration         `json:"tolerations"`
 }
 
 // CertificateIssuer defines the desired state of the CertificateIssuer component
