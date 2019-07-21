3c5ae2e24469ac47539f280bda510ed38d6ca6a5
Michael D. Elder <mdelder@us.ibm.com>
2019-07-20 21:53:55 -0400

    Update missing copyrights to correct linting

--- pkg/apis/klusterlet/v1alpha1/register.go
+++ pkg/apis/klusterlet/v1alpha1/register.go
@@ -1,4 +1,12 @@
-// NOTE: Boilerplate only.  Ignore this file.
+/*
+ * IBM Confidential
+ * OCO Source Materials
+ * 5737-E67
+ * (C) Copyright IBM Corporation 2018 All Rights Reserved
+ * The source code for this program is not published or otherwise divested of its trade secrets, irrespective of what has been deposited with the U.S. Copyright Office.
+ */
+ 
+ // NOTE: Boilerplate only.  Ignore this file.
 
 // Package v1alpha1 contains API Schema definitions for the klusterlet v1alpha1 API group
 // +k8s:deepcopy-gen=package,register
