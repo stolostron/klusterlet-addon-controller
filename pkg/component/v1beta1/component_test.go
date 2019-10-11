353588ebebd51fcb977df8060e3fcde0c1356070
Yumeng Lu <Yumeng.Lu@ibm.com>
2019-10-11 15:03:56 -0400

    adding unit tests for migration, connect manager, and component (#117)

---
+++ pkg/component/v1beta1/component_test.go
@@ -0,0 +1,113 @@
+// Package v1beta1 of component Defines the Reconciliation logic and required setup for component operator.
+// IBM Confidential
+// OCO Source Materials
+// 5737-E67
+// (C) Copyright IBM Corporation 2019 All Rights Reserved
+// The source code for this program is not published or otherwise divested of its trade secrets, irrespective of what has been deposited with the U.S. Copyright Office.
+package v1beta1
+
+import (
+	"context"
+	"testing"
+
+	// fakecrdclientset "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset/fake"
+	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+	extensionsv1beta1 "k8s.io/api/extensions/v1beta1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	multicloudv1beta1 "github.ibm.com/IBMPrivateCloud/ibm-klusterlet-operator/pkg/apis/multicloud/v1beta1"
+)
+
+var (
+	namespace = "multicluster-endpoint"
+)
+
+func newTestDeployment(name string) *extensionsv1beta1.Deployment {
+	deployment := &extensionsv1beta1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+	}
+	return deployment
+}
+
+func newInstance() *multicloudv1beta1.Endpoint {
+	instance := &multicloudv1beta1.Endpoint{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "endpoint",
+			Namespace: namespace,
+		},
+		Spec: multicloudv1beta1.EndpointSpec{
+			Version:         "3.2.1",
+			ImagePullPolicy: "Always",
+			ImagePullSecret: "image-pull",
+		},
+	}
+	return instance
+}
+
+func TestCreateReconcile(t *testing.T) {
+	objs := []runtime.Object{}
+	cl := fake.NewFakeClient(objs...)
+	instance := newInstance()
+	scheme := scheme.Scheme
+	scheme.AddKnownTypes(multicloudv1beta1.SchemeGroupVersion, instance)
+
+	err := Reconcile(instance, cl, scheme)
+	assert.NoError(t, err, "Create component reconcile should success")
+
+	deploymentComponentOperator := &extensionsv1beta1.Deployment{}
+	err = cl.Get(context.TODO(), types.NamespacedName{Name: instance.Name + "-component-operator", Namespace: namespace}, deploymentComponentOperator)
+	assert.NoError(t, err, "component deployment should be created")
+
+	clusterRole := &rbacv1.ClusterRole{}
+	err = cl.Get(context.TODO(), types.NamespacedName{Name: instance.Name + "-component-operator"}, clusterRole)
+	assert.NoError(t, err, "clusterRole should be created")
+
+	serviceAccount := &corev1.ServiceAccount{}
+	err = cl.Get(context.TODO(), types.NamespacedName{Name: instance.Name + "-component-operator", Namespace: namespace}, serviceAccount)
+	assert.NoError(t, err, "serviceAccount should be created")
+
+	clusterRoleBinding := &rbacv1.ClusterRoleBinding{}
+	err = cl.Get(context.TODO(), types.NamespacedName{Name: instance.Name + "-component-operator"}, clusterRoleBinding)
+	assert.NoError(t, err, "clusterRoleBinding should be created")
+}
+
+func TestUpdateReconcile(t *testing.T) {
+	instance := newInstance()
+
+	deploymentComponentOperator := newTestDeployment(instance.Name + "-component-operator")
+
+	objs := []runtime.Object{deploymentComponentOperator}
+	cl := fake.NewFakeClient(objs...)
+
+	scheme := scheme.Scheme
+	scheme.AddKnownTypes(multicloudv1beta1.SchemeGroupVersion, instance)
+
+	err := Reconcile(instance, cl, scheme)
+	assert.NoError(t, err, "Update component reconcile should success")
+
+	foundDeployment := &extensionsv1beta1.Deployment{}
+	err = cl.Get(context.TODO(), types.NamespacedName{Name: deploymentComponentOperator.Name, Namespace: deploymentComponentOperator.Namespace}, foundDeployment)
+	assert.NoError(t, err, "GET component deployment should success")
+
+	assert.Equal(t, foundDeployment.Spec.Template.Spec.Containers[0].ImagePullPolicy, instance.Spec.ImagePullPolicy, "update component reconcile should update imagepullpolicy")
+	assert.Equal(t, foundDeployment.Spec.Template.Spec.ImagePullSecrets[0].Name, instance.Spec.ImagePullSecret, "update component reconcile should update imagepullsecret")
+}
+
+// The fakeclientset from apiextensions-apiserver pkg has a problem and it cannot match real clientset
+// func TestInstallComponentCRDs(t *testing.T) {
+// 	objs := []runtime.Object{}
+// 	clientset := fakecrdclientset.NewSimpleClientset(objs...)
+// 	err := InstallComponentCRDs(clientset)
+// 	if err != nil {
+// 		t.Fatalf("InstallComponentCRDs error: (%v)", err)
+// 	}
+// }
---
+++ pkg/connmgr/v1beta1/connmgr_test.go
@@ -0,0 +1,184 @@
+// Package v1beta1 of connmgrs Defines the Reconciliation logic and required setup for component operator.
+// IBM Confidential
+// OCO Source Materials
+// 5737-E67
+// (C) Copyright IBM Corporation 2019 All Rights Reserved
+// The source code for this program is not published or otherwise divested of its trade secrets, irrespective of what has been deposited with the U.S. Copyright Office.
+package v1beta1
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	extensionsv1beta1 "k8s.io/api/extensions/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	multicloudv1beta1 "github.ibm.com/IBMPrivateCloud/ibm-klusterlet-operator/pkg/apis/multicloud/v1beta1"
+)
+
+var (
+	namespace = "multicluster-endpoint"
+)
+
+func newTestDeployment(name string) *extensionsv1beta1.Deployment {
+	deployment := &extensionsv1beta1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Status: extensionsv1beta1.DeploymentStatus{
+			Conditions: []extensionsv1beta1.DeploymentCondition{extensionsv1beta1.DeploymentCondition{
+				Type:   "Available",
+				Status: "True",
+			}},
+		},
+	}
+	return deployment
+}
+
+func newInstance() *multicloudv1beta1.Endpoint {
+	instance := &multicloudv1beta1.Endpoint{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "endpoint",
+			Namespace: namespace,
+		},
+		Spec: multicloudv1beta1.EndpointSpec{
+			Version: "3.2.1",
+		},
+	}
+	return instance
+}
+
+func newInstanceInDeletion() *multicloudv1beta1.Endpoint {
+	instance := &multicloudv1beta1.Endpoint{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:              "endpoint",
+			Namespace:         namespace,
+			DeletionTimestamp: &metav1.Time{},
+			Finalizers:        []string{"endpoint-connmgr"},
+		},
+		Spec: multicloudv1beta1.EndpointSpec{
+			Version:         "3.2.1",
+			ImagePullPolicy: "Always",
+			ImagePullSecret: "image-pull",
+		},
+	}
+	return instance
+}
+
+func newConnmgr() *multicloudv1beta1.ConnectionManager {
+	connmgr := &multicloudv1beta1.ConnectionManager{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "endpoint-connmgr",
+			Namespace: namespace,
+		},
+	}
+	return connmgr
+}
+
+func TestCreateReconcile(t *testing.T) {
+	instance := newInstance()
+	connmgr := &multicloudv1beta1.ConnectionManager{}
+
+	scheme := scheme.Scheme
+	scheme.AddKnownTypes(multicloudv1beta1.SchemeGroupVersion, instance)
+	scheme.AddKnownTypes(multicloudv1beta1.SchemeGroupVersion, connmgr)
+
+	objs := []runtime.Object{instance}
+	cl := fake.NewFakeClient(objs...)
+
+	res, err := Reconcile(instance, cl, scheme)
+	assert.NoError(t, err, "CREATE connmgr reconcile should success")
+	assert.False(t, res, "CREATE connmgr reconcile should return false")
+
+	err = cl.Get(context.TODO(), types.NamespacedName{Name: instance.ObjectMeta.Name + "-connmgr", Namespace: namespace}, connmgr)
+	assert.NoError(t, err, "connmgr CR should be created")
+}
+
+func TestFinalizeReconcile(t *testing.T) {
+	instance := newInstanceInDeletion()
+	connmgr := &multicloudv1beta1.ConnectionManager{}
+
+	scheme := scheme.Scheme
+	scheme.AddKnownTypes(multicloudv1beta1.SchemeGroupVersion, instance)
+	scheme.AddKnownTypes(multicloudv1beta1.SchemeGroupVersion, connmgr)
+
+	objs := []runtime.Object{instance}
+	cl := fake.NewFakeClient(objs...)
+
+	res, err := Reconcile(instance, cl, scheme)
+	assert.NoError(t, err, "FINALIZE connmgr reconcile should success")
+	assert.False(t, res, "FINALIZE connmgr reconcile should return false")
+
+	assert.Empty(t, instance.Finalizers, "Finalizer shoule be removed")
+}
+
+func TestUpdateReconcile(t *testing.T) {
+	instance := newInstance()
+	connmgr := newConnmgr()
+
+	scheme := scheme.Scheme
+	scheme.AddKnownTypes(multicloudv1beta1.SchemeGroupVersion, instance)
+	scheme.AddKnownTypes(multicloudv1beta1.SchemeGroupVersion, connmgr)
+
+	objs := []runtime.Object{connmgr}
+	cl := fake.NewFakeClient(objs...)
+
+	res, err := Reconcile(instance, cl, scheme)
+	assert.NoError(t, err, "UPDATE connmgr reconcile should success")
+	assert.False(t, res, "UPDATE connmgr reconcile should return false")
+
+	err = cl.Get(context.TODO(), types.NamespacedName{Name: instance.ObjectMeta.Name + "-connmgr", Namespace: namespace}, connmgr)
+	assert.NoError(t, err, "connmgr CR should be created")
+	assert.Equal(t, connmgr.Spec.ClusterName, instance.Spec.ClusterName, "connmgr CR ClusterName should be updated")
+	assert.Equal(t, connmgr.Spec.ClusterNamespace, instance.Spec.ClusterNamespace, "connmgr CR ClusterNamespace should be updated")
+	assert.Equal(t, connmgr.Spec.BootStrapConfig, instance.Spec.BootStrapConfig, "connmgr CR BootStrapConfig should be updated")
+	assert.Equal(t, connmgr.Spec.ImagePullSecret, instance.Spec.ImagePullSecret, "connmgr CR ImagePullSecret should be updated")
+}
+
+func TestDeleteReconcile(t *testing.T) {
+	instance := newInstanceInDeletion()
+	connmgr := newConnmgr()
+
+	scheme := scheme.Scheme
+	scheme.AddKnownTypes(multicloudv1beta1.SchemeGroupVersion, instance)
+	scheme.AddKnownTypes(multicloudv1beta1.SchemeGroupVersion, connmgr)
+
+	objs := []runtime.Object{connmgr}
+	cl := fake.NewFakeClient(objs...)
+
+	res, err := Reconcile(instance, cl, scheme)
+	assert.NoError(t, err, "DELETE connmgr reconcile should success")
+	assert.Equal(t, res, true, "DELETE connmgr reconcile should return true")
+
+	err = cl.Get(context.TODO(), types.NamespacedName{Name: instance.ObjectMeta.Name + "-connmgr", Namespace: namespace}, connmgr)
+	assert.Error(t, err, "connmgr CR should be deleted")
+}
+
+func TestIsReady(t *testing.T) {
+	instance := newInstanceInDeletion()
+	deployment := newTestDeployment(instance.ObjectMeta.Name + "-connmgr")
+
+	objs := []runtime.Object{deployment}
+	cl := fake.NewFakeClient(objs...)
+
+	res, err := IsReady(instance, cl)
+	assert.NoError(t, err, "IsReady should success")
+	assert.Equal(t, res, true, "IsReady should return true")
+}
+
+func TestIsNotReady(t *testing.T) {
+	instance := newInstanceInDeletion()
+
+	objs := []runtime.Object{}
+	cl := fake.NewFakeClient(objs...)
+
+	res, err := IsReady(instance, cl)
+	assert.NoError(t, err, "IsReady should success")
+	assert.False(t, res, "IsReady should return false")
+}
