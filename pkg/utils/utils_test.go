278044450456bb3335b2b3772d213b47f4401ea6
Xiaomeng Xie <Diane.Xiaomeng.Xie@ibm.com>
2019-11-14 16:37:38 -0500

    remove UUID in copyright for ibm_klusteret-operator (#149)
    * remove UUID

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -1,7 +1,6 @@
 //Package utils contains common utility functions that gets call by many differerent packages
 // IBM Confidential
 // OCO Source Materials
-// 5737-E67
 // (C) Copyright IBM Corporation 2019 All Rights Reserved
 // The source code for this program is not published or otherwise divested of its trade secrets, irrespective of what has been deposited with the U.S. Copyright Office.
 package utils
@@ -10,7 +9,6 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 )
 
