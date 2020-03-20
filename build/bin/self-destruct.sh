84e81b93f4e5b49638e1da23b82953cc85622e69
Hanqiu Zhang <31426239+hanqiuzh@users.noreply.github.com>
2020-03-20 16:36:54 -0400

    update destruct script (#35)
    * update destruct script
    
    * add back endpoints in crd deletion
    
    * completely remove helmreleases in self-destruct.sh

--- build/bin/self-destruct.sh
+++ build/bin/self-destruct.sh
@@ -12,38 +12,19 @@ kubectl delete deployment ibm-multicluster-endpoint-operator -n ${OPERATOR_NAMES
 
 # Force delete all component CRDs if they still exist
 component_crds=(
-	helmreleases.helm.bitnami.com
 	applicationmanagers.multicloud.ibm.com
 	certmanagers.multicloud.ibm.com
+	certpoliciescontroller.multicloud.ibm.com
+	ciscontrollers.multicloud.ibm.com
 	connectionmanagers.multicloud.ibm.com
-	meterings.multicloud.ibm.com
-	mongodbs.multicloud.ibm.com
+	iampoliciescontroller.multicloud.ibm.com
 	policycontrollers.multicloud.ibm.com
 	searchcollectors.multicloud.ibm.com
 	serviceregistries.multicloud.ibm.com
-	tillers.multicloud.ibm.com
 	topologycollectors.multicloud.ibm.com
 	workmanagers.multicloud.ibm.com
 	endpoints.multicloud.ibm.com
 )
-
-# Loop through all namespaces and delete all helmreleases.helm.bitnami.com CRD resources (backup in case operator fails)
-for namespace in `kubectl get namespaces -o name`; do
-    ns=`echo $namespace | cut -d "/" -f 2`
-    echo "Processing namespace ${ns}"
-    for cr in `kubectl get helmrelease -n ${ns} -o name`; do
-        echo "Attempt to remove finalizer for CR ${cr}"
-        kubectl patch ${cr} --type="json" -p '[{"op": "remove", "path":"/metadata/finalizers"}]'
-        echo "Attempt to delete CR ${cr} in namespace ${ns}"
-        kubectl delete ${cr} -n ${ns}
-    done
-done
-
-# special case for meterings.multicloud.ibm.com
-for resource in `kubectl get meterings.multicloud.ibm.com -n kube-system -o name`; do
-	kubectl delete ${resource} -n kube-system --timeout=15s
-	kubectl patch ${resource} -n kube-system --type="json" -p '[{"op": "remove", "path":"/metadata/finalizers"}]'
-done
 
 for crd in "${component_crds[@]}"; do
 	echo "force delete all CustomResourceDefination ${crd} resources..."
