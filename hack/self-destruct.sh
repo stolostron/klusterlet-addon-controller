59ff3c5e881ab219b0a2fc059983d13a1882dd39
Hanqiu Zhang <31426239+hanqiuzh@users.noreply.github.com>
2020-07-07 13:08:14 -0400

    Update self-destruct.sh (#165)
    * update deletion script for klusterlet & crds
    
    * remove connection manager

--- hack/self-destruct.sh
+++ hack/self-destruct.sh
@@ -4,38 +4,43 @@
 ###############################################################################
 
 if [ -z "${OPERATOR_NAMESPACE}" ]; then
-	OPERATOR_NAMESPACE="klusterlet"
+	OPERATOR_NAMESPACE="open-cluster-management-agent-addon"
 fi
 
-# Delete all klusterlets.agent.open-cluster-management.io
-kubectl delete klusterlets.agent.open-cluster-management.io -n ${OPERATOR_NAMESPACE}  --all --timeout=60s
+if [ -z "${KLUSTERLET_NAMESPACE}" ]; then
+	KLUSTERLET_NAMESPACE="open-cluster-management-agent"
+fi
 
-# Delete Deployment
-kubectl delete deployment klusterlet-operator -n ${OPERATOR_NAMESPACE}
+KUBECTL=oc
+
+# Force delete klusterlet
+echo "attempt to delete klusterlet"
+${KUBECTL} delete klusterlet klusterlet --timeout=60s
+${KUBECTL} delete namespace ${KLUSTERLET_NAMESPACE} --wait=false
+echo "force removing klusterlet"
+${KUBECTL} patch klusterlet klusterlet --type="json" -p '[{"op": "remove", "path":"/metadata/finalizers"}]'
 
 # Force delete all component CRDs if they still exist
 component_crds=(
 	applicationmanagers.agent.open-cluster-management.io
 	certpolicycontrollers.agent.open-cluster-management.io
 	cispolicycontrollers.agent.open-cluster-management.io
-	connectionmanagers.agent.open-cluster-management.io
 	iampolicycontrollers.agent.open-cluster-management.io
 	policycontrollers.agent.open-cluster-management.io
 	searchcollectors.agent.open-cluster-management.io
 	workmanagers.agent.open-cluster-management.io
-	klusterlets.agent.open-cluster-management.io
 )
 
 for crd in "${component_crds[@]}"; do
 	echo "force delete all CustomResourceDefination ${crd} resources..."
-	for resource in `kubectl get ${crd} -o name -n ${OPERATOR_NAMESPACE}`; do
+	for resource in `${KUBECTL} get ${crd} -o name -n ${OPERATOR_NAMESPACE}`; do
 		echo "attempt to delete ${crd} resource ${resource}..."
-		kubectl delete ${resource} -n ${OPERATOR_NAMESPACE} --timeout=15s
+		${KUBECTL} delete ${resource} -n ${OPERATOR_NAMESPACE} --timeout=30s
 		echo "force remove ${crd} resource ${resource}..."
-		kubectl patch ${resource} -n ${OPERATOR_NAMESPACE} --type="json" -p '[{"op": "remove", "path":"/metadata/finalizers"}]'
+		${KUBECTL} patch ${resource} -n ${OPERATOR_NAMESPACE} --type="json" -p '[{"op": "remove", "path":"/metadata/finalizers"}]'
 	done
 	echo "force delete all CustomResourceDefination ${crd} resources..."
-	kubectl delete crd ${crd}
+	${KUBECTL} delete crd ${crd}
 done
 
-kubectl delete namespace ${OPERATOR_NAMESPACE}
+${KUBECTL} delete namespace ${OPERATOR_NAMESPACE}
