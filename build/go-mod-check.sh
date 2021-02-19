4431896415e0c83bb08bc654197228e75ff6f63a
Leena Jawale <ljawale@redhat.com>
2021-02-19 14:35:09 -0500

    update makefile

--- build/go-mod-check.sh
+++ build/go-mod-check.sh
@@ -3,11 +3,10 @@
 set -e
 
 echo "Running go mod tidy check..."
-TRAVIS_BRANCH=$1
 go mod tidy
 STATUS=$( git status --porcelain go.mod go.sum )
 
-if [[ ! -z "$STATUS"  &&  "x${TRAVIS_BRANCH}" != "x" ]]; then
+if [[ ! -z "$STATUS" != "x" ]]; then
     echo "FAILED: 'go mod tidy' modified go.mod and/or go.sum"
     exit 1
 fi
