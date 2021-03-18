5ca299180dc498a056b242e25de5a06f5180b44c
Han Zhang <hanzhang@redhat.com>
2021-03-18 10:51:30 -0400

    add upgrade deletion previous role/rolebinding support (#21)
    * add upgrade deletion previous role/rolebinding support
    
    * func test fix
    
    * fix naming

--- pkg/controller/klusterletaddon/klusterlet_addon_crs.go
+++ pkg/controller/klusterletaddon/klusterlet_addon_crs.go
@@ -37,10 +37,12 @@ import (
 	"github.com/open-cluster-management/library-go/pkg/templateprocessor"
 	ocinfrav1 "github.com/openshift/api/config/v1"
 	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/wait"
 	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
@@ -92,11 +94,77 @@ var merger applier.Merger = func(current,
 	return current, update
 }
 
+//deleteOutDatedRoleRoleBindings deletes old role/rolebinding with klusterletaddonconfig ownerRef (controller).
+//it returns nil if no role/rolebinding exist, and it returns error when failed to delete the role/rolebinding
+func deleteOutDatedRoleRoleBinding(
+	addon addons.KlusterletAddon,
+	klusterletaddonconfig *agentv1.KlusterletAddonConfig,
+	client client.Client,
+) error {
+	if klusterletaddonconfig == nil {
+		return nil
+	}
+	// name used in previous addon role & rolebinding
+	name := klusterletaddonconfig.Name + "-" + addon.GetAddonName()
+	// check if the role/rolebinding exist
+	role := &rbacv1.Role{}
+	rolebinding := &rbacv1.RoleBinding{}
+	objs := make([]runtime.Object, 0)
+	objs = append(objs, role)
+	objs = append(objs, rolebinding)
+	var retErr error
+	retErr = nil
+	for _, o := range objs {
+		if err := client.Get(
+			context.TODO(),
+			types.NamespacedName{
+				Name:      name,
+				Namespace: klusterletaddonconfig.Namespace,
+			}, o); err != nil && errors.IsNotFound(err) {
+			continue
+		} else if err != nil {
+			retErr = err
+			continue
+		}
+
+		// verify ownerRef
+		if objMetaAccessor, ok := o.(metav1.ObjectMetaAccessor); !ok {
+			log.V(2).Info("Failed to get ObjectMeta")
+			continue
+		} else {
+			ownerRef := metav1.GetControllerOf(objMetaAccessor.GetObjectMeta())
+			if ownerRef == nil {
+				log.V(2).Info("No controller reference of the role, skipping")
+				continue
+			}
+			ownerGV, err := schema.ParseGroupVersion(ownerRef.APIVersion)
+			if err != nil {
+				log.V(2).Info("Failed to get group from object ownerRef")
+				continue
+			}
+			if ownerRef.Kind != klusterletaddonconfig.Kind ||
+				ownerRef.Name != klusterletaddonconfig.Name ||
+				ownerGV.Group != klusterletaddonconfig.GroupVersionKind().Group {
+				log.V(2).Info("Object is not owned by klusterletaddonconfig. Skipping")
+				continue
+			}
+		}
+
+		if err := client.Delete(context.TODO(), o); err != nil && !errors.IsNotFound(err) {
+			retErr = err
+			continue
+		}
+	}
+
+	return retErr
+}
+
 func createOrUpdateHubKubeConfigResources(
 	klusterletaddonconfig *agentv1.KlusterletAddonConfig,
 	r *ReconcileKlusterletAddon,
 	addon addons.KlusterletAddon) error {
 	componentName := addon.GetAddonName()
+
 	//Create the values for the yamls
 	config := struct {
 		ManagedClusterName      string
@@ -139,6 +207,10 @@ func createOrUpdateHubKubeConfigResources(
 	)
 	if err != nil {
 		return err
+	}
+	// delete old role & rolebindings created in previous releases
+	if err := deleteOutDatedRoleRoleBinding(addon, klusterletaddonconfig, r.client); err != nil {
+		log.Info("Failed to delete outdated role/rolebinding. Skipping.", "error message:", err)
 	}
 
 	return nil
