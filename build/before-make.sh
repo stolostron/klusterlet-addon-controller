ae092507ad12ea09949add9cad0d6a76bb3133f4
Leena Jawale <ljawale@redhat.com>
2021-04-30 18:04:32 -0400

    add signoff hook (#35)
    Signed-off-by: Leena Jawale <ljawale@redhat.com>

--- build/before-make.sh
+++ build/before-make.sh
@@ -10,3 +10,5 @@
 ###############################################################################
 
 ln -sf ../../build/git-hooks/pre-commit .git/hooks/pre-commit
+ln -sf ../../build/git-hooks/commit-msg .git/hooks/commit-msg
+
