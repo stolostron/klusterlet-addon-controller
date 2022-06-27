892b97d21b4c986103611683807229024d90f8da
Brian Smith <60108055+smithbw88@users.noreply.github.com>
2022-06-27 16:41:06 -0400

    2.4.5 => 2.4.6
    Signed-off-by: Brian Smith briansmi@redhat.com

--- pkg/controller/klusterletaddon/klusterlet_addon_components_test.go
+++ pkg/controller/klusterletaddon/klusterlet_addon_components_test.go
@@ -39,11 +39,11 @@ func setup() error {
 			Kind:       "ConfigMap",
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "test-configmap-2.4.5",
+			Name:      "test-configmap-2.4.6",
 			Namespace: "test-namespace",
 			Labels: map[string]string{
 				"ocm-configmap-type":  "image-manifest",
-				"ocm-release-version": "2.4.5",
+				"ocm-release-version": "2.4.6",
 			},
 		},
 		Data: map[string]string{
