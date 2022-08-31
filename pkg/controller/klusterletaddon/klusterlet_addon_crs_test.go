9880687f39531b7d4cac58f69c6a9c7f1cf78b35
Brian Smith <60108055+smithbw88@users.noreply.github.com>
2022-08-31 14:59:21 -0400

    2.3.12 => 2.3.13 (#157)
    * 2.3.12 => 2.3.13
    
    Signed-off-by: Brian Smith briansmi@redhat.com
    
    * 2.3.12 => 2.3.13
    
    Signed-off-by: Brian Smith briansmi@redhat.com
    
    * 2.3.12 => 2.3.13
    
    Signed-off-by: Brian Smith briansmi@redhat.com
    
    * 2.3.12 => 2.3.13
    
    Signed-off-by: Brian Smith briansmi@redhat.com
    
    * 2.3.12 => 2.3.13
    
    Signed-off-by: Brian Smith briansmi@redhat.com
    
    * 2.3.12 => 2.3.13
    
    Signed-off-by: Brian Smith briansmi@redhat.com
    
    Signed-off-by: Brian Smith briansmi@redhat.com

--- pkg/controller/klusterletaddon/klusterlet_addon_crs_test.go
+++ pkg/controller/klusterletaddon/klusterlet_addon_crs_test.go
@@ -58,11 +58,11 @@ func Test_syncManifestWorkCRs(t *testing.T) {
 			Kind:       "ConfigMap",
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "test-configmap-2.3.12",
+			Name:      "test-configmap-2.3.13",
 			Namespace: "test-namespace",
 			Labels: map[string]string{
 				"ocm-configmap-type":  "image-manifest",
-				"ocm-release-version": "2.3.12",
+				"ocm-release-version": "2.3.13",
 			},
 		},
 		Data: map[string]string{
