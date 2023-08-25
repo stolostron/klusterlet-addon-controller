21fcf32a88b845c67d0dcc47e0d86002c182d5d8
Zhiwei Yin <zyin@redhat.com>
2023-08-25 15:16:12 +0800

    not delete addon when cluster is not found or deleting (#213)
    Signed-off-by: Zhiwei Yin <zyin@redhat.com>

--- test/e2e/managedclusteraddon_test.go
+++ test/e2e/managedclusteraddon_test.go
@@ -8,7 +8,6 @@ import (
 	"fmt"
 	"time"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/rand"
 	addonv1alpha1 "open-cluster-management.io/api/addon/v1alpha1"
@@ -126,48 +125,6 @@ var _ = Describe("managedClusterAddon test", func() {
 					}
 					if addon.Spec.InstallNamespace != "default" {
 						return fmt.Errorf("expected the addon to be installed in default namespace, but got %s", addon.Spec.InstallNamespace)
-					}
-					return nil
-				}, 60*time.Second, 5*time.Second).ShouldNot(HaveOccurred())
-			})
-		})
-		It("test managedCluster delete", func() {
-			assertManagedClusterNamespace(managedClusterName)
-
-			addonConfig := newKlusterletAddonConfig(managedClusterName)
-			By("create klusterletAddonConfig", func() {
-				err := kubeClient.Create(context.TODO(), addonConfig)
-				Expect(err).ToNot(HaveOccurred())
-			})
-
-			By("check if all addons are installed", func() {
-				Eventually(func() error {
-					addonList := &addonv1alpha1.ManagedClusterAddOnList{}
-					err := kubeClient.List(context.TODO(), addonList, &client.ListOptions{Namespace: managedClusterName})
-					if err != nil {
-						return err
-					}
-					if len(addonList.Items) != 6 {
-						return fmt.Errorf("expected 6 addons, but got %v", len(addonList.Items))
-					}
-					return nil
-				}, 60*time.Second, 5*time.Second).ShouldNot(HaveOccurred())
-			})
-
-			By("delete managedCluster", func() {
-				err := hubClusterClient.ClusterV1().ManagedClusters().Delete(context.TODO(), managedClusterName, metav1.DeleteOptions{})
-				Expect(err).ToNot(HaveOccurred())
-			})
-
-			By("check if all addons are deleted", func() {
-				Eventually(func() error {
-					addonList := &addonv1alpha1.ManagedClusterAddOnList{}
-					err := kubeClient.List(context.TODO(), addonList, &client.ListOptions{Namespace: managedClusterName})
-					if err != nil {
-						return err
-					}
-					if len(addonList.Items) != 0 {
-						return fmt.Errorf("expected 0 addons, but got %v", len(addonList.Items))
 					}
 					return nil
 				}, 60*time.Second, 5*time.Second).ShouldNot(HaveOccurred())
